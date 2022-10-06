71918f260f08206eafd85c726e6aecf35d38fcba
Jean-Damien Durand <jeandamiendurand@free.fr>
2022-10-06 19:42:07 +0200

    dev+

--- perlpatch.bat
+++ perlpatch.bat
@@ -15,4 +15,4 @@ REM Installation
 REM cscript "%1\replace.vbs" "%2\win32\Makefile" "INST_TOP\s*=\s*\$\(INST_DRV\)\\perl" "INST_TOP	= $(INST_DRV)\perl"
 cscript "%1" "%2\win32\Makefile" "#\s*INST_VER" "INST_VER"
 cscript "%1" "%2\win32\Makefile" "#\s*INST_ARCH" "INST_ARCH"
-cscript "%1" "%2\win32\Makefile" "#\s*USE_SITECUST" "USE_SITECUST"
+REM cscript "%1" "%2\win32\Makefile" "#\s*USE_SITECUST" "USE_SITECUST"
